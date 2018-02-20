0b07e2eba372575ae2eb233019f1a10020f2ec12
Isaac Getto <isaac.getto@gmail.com>
2018-02-20 12:26:20 -0500

    use macros

--- devboard/led_control.c
+++ devboard/led_control.c
@@ -2,10 +2,14 @@
 
 #include <avr/io.h>
 
-// LED1 = PC5
-// LED2 = PC2
-// Button = PD1
-// Switch = PD5
+#define LED1 PC5
+#define LED2 PC2
+#define BTN PD1
+#define SW PD5
+
+#define read_bit(reg, bit) (reg & (1 << bit))
+#define clear_bit(reg, bit) (reg &= ~(1 << bit))
+#define set_bit(reg, bit) (reg |= (1 << bit))
 
 
 int main(void) {
@@ -14,35 +18,39 @@ int main(void) {
 
   // Data Direction Register C is used 
   // to enable output on PC5 and PC2
-  DDRC |= (1 << PC5);
-  DDRC |= (1 << PC2);
+  set_bit(DDRC, LED1);
+  set_bit(DDRC, LED2);
 
   // Data Direction Register D is used
   // to enable input on PD1 and PD5
   //
   // Port D is used to enable the 
   // internal pullup resistors
-  DDRD &= ~(1 << PD1);
-  DDRD &= ~(1 << PD5);
-  PORTD |= (1 << PD1);
-  PORTD |= (1 << PD5);
+  clear_bit(DDRD, SW);
+  clear_bit(DDRD, BTN);
+  set_bit(PORTD, SW);
+  set_bit(PORTD, BTN);
 
 
   // ------ Event loop ------ //
   while (1) {
 
-    if (PIND & (1 << PD5)) {
-      PORTC &= ~(1 << PC2);
+
+    // if the button is pressed
+    // then turn LED1 on
+    if (read_bit(PIND, BTN)) {
+      clear_bit(PORTC, LED1);
     } else {
-      PORTC |= (1 << PC2);
+      set_bit(PORTC, LED1);
     }
 
-    if (PIND & (1 << PD1)) {
-      PORTC &= ~(1 << PC5);
+    // if the switch has is flipped
+    // then turn the LED2 on
+    if (read_bit(PIND, SW)) {
+      clear_bit(PORTC, LED2);
     } else {
-      PORTC |= (1 << PC5);
+      set_bit(PORTC, LED2);
     }
-
   }       
 
   return 0;
